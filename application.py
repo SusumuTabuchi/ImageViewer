9b3f2db8757763090302c261c7bb3cb1d454d782
Susunu Tabuchi <swk22711@gmail.com>
2020-01-13 23:33:58 +0900

    話数combobox OK

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ class ApplicationInfo:
         return [os.path.basename(m) for m in glob.glob(os.path.join(archive_directory, "*"))]
     
     def set_manga_numbers(self, archive_directory, manga_title):
-        return tuple([os.path.basename(m) for m in glob.glob(os.path.join(archive_directory, manga_title + os.sep + "*"))])
+        self.manga_numbers = tuple([os.path.basename(m) for m in glob.glob(os.path.join(archive_directory, manga_title + os.sep + "*"))])
 
 
 class ImageViewer():
@@ -38,33 +38,29 @@ class ImageViewer():
         self.init(root)
     
     def init(self, root=None):
-        ### window
-        ### name : window_main
+        ### window_main
         root.title(self.setting_info.title + " " + self.setting_info.version)
         root.geometry(self.setting_info.width + "x" + self.setting_info.height)
 
-        ### Frame
-        ### name : Frame_Explorer
-        Frame_Explorer = ttk.Labelframe(root, text="漫画を選択する",
+        ## Frame_Explorer
+        self.Frame_Explorer = ttk.Labelframe(root, text="漫画を選択する",
                                         borderwidth=7, relief="ridge")
-        Frame_Explorer.grid(column=0, row=0)
-        ### widget
-        label_manga = ttk.Label(Frame_Explorer, text="manga", padding=5)
-        label_manga.grid(column=0, row=0)
+        self.Frame_Explorer.grid(column=0, row=0)
+        # widget
+        self.label_manga = ttk.Label(self.Frame_Explorer, text="title", padding=5)
+        self.label_manga.grid(column=0, row=0)
 
         self.value_manga_title = tk.StringVar()
-        combobox_mangatitle = ttk.Combobox(Frame_Explorer, textvariable=self.value_manga_title, width=20)
-        combobox_mangatitle.bind("<<ComboboxSelected>>", self.select_combobox_manga_title)
-        combobox_mangatitle["values"] = self.application_info.manga_titles
-        combobox_mangatitle.grid(column=1, row=0)
+        self.combobox_mangatitle = ttk.Combobox(self.Frame_Explorer, textvariable=self.value_manga_title, width=20)
+        self.combobox_mangatitle.bind("<<ComboboxSelected>>", self.select_combobox_manga_title)
+        self.combobox_mangatitle["values"] = self.application_info.manga_titles
+        self.combobox_mangatitle.grid(column=1, row=0)
 
-        label_number = ttk.Label(Frame_Explorer, text="話数", padding=5)
-        label_number.grid(column=0, row=1)
+        self.label_number = ttk.Label(self.Frame_Explorer, text="話数", padding=5)
+        self.label_number.grid(column=0, row=1)
 
         self.value_manga_number = tk.StringVar()
-        self.combobox_manganumber = ttk.Combobox(Frame_Explorer, textvariable=self.value_manga_number, width=20)
-        # combobox_manganumber.bind("<<ComboboxSelected>>", self.select_combobox_manga_title)
-        # combobox_manganumber["values"] = self.application_info.manga_numbers
+        self.combobox_manganumber = ttk.Combobox(self.Frame_Explorer, textvariable=self.value_manga_number, width=20)
         self.combobox_manganumber.grid(column=1, row=1)
 
 
@@ -77,40 +73,19 @@ class ImageViewer():
         label1 = ttk.Label(Frame_commonSetting, text="label1")
         label1.grid(column=0, row=0)
 
-
-    
-    def create_pane(self):
-        self.pw_main = tk.PanedWindow(self, orient="horizontal", bg="blue")
-        self.pw_main.pack(expand=True, fill=tk.BOTH, side="left")
-
-        self.pw_left = tk.PanedWindow(self.pw_main, bg="cyan", orient='vertical')
-        self.pw_main.add(self.pw_left)
-        self.pw_right = tk.PanedWindow(self.pw_main, bg="yellow", orient='vertical')
-        self.pw_main.add(self.pw_right)
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self, text="Hello World\n(click me)",
-                                  command=self.say_hi, bg="yellow")
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
-
     def popup_error(self, title, message):
         tk.messagebox.showerror(title, message)
+
+
 
     ###
     ### application event method
     ###
-    def say_hi(self):
-        print("hi there, everyone!")
-    
     def select_combobox_manga_title(self, event):
         self.application_info.manga_title = self.value_manga_title.get()
         self.application_info.set_manga_numbers(self.application_info.archive_directory, self.application_info.manga_title)
         self.combobox_manganumber["values"] = self.application_info.manga_numbers
-        print(self.application_info.manga_title)
+        self.combobox_manganumber.set("")
 
 
 
@@ -123,5 +98,17 @@ if __name__ == "__main__":
 
 # Todo
 """
-漫画選択後に話数フォルダをcomboboxに表示する
+OK 漫画選択後に話数フォルダをcomboboxに表示する
+
+共通設定：ラジオボタン１、２
+　⇒ 基数偶数入れ替え
+　　　　：チェックボックス
+　⇒ 一枚表示（デフォルトは2枚表示）
+
+イメージ表示枠：右・左
+　⇒ 表示の大きさは左右同じ
+　⇒ 画像自体は真ん中寄せで作成する
+
+ラベル：イメージ表示枠の下にススム戻るボタン
+　⇒ 進める。戻れる場合のみ表示、できない場合は薄く表示され、クリックはできないようにする
 """
